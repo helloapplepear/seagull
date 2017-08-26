835b4481a45d2a84d9118b96b97be1cd97318c95
david li <276758175@qq.com>
2017-08-26 21:49:40 +0800

    初始提交

---
+++ application/views/welcome_message.php
@@ -0,0 +1,13 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>static</title>
+  </head>
+  <body>
+    <div id="app">hello world</div>
+    <?php $this->load->helper('url');
+
+    ?>
+    <script src="<? echo base_url('/static/dist/build.js');?>"></script>
+  </body>
+</html>
